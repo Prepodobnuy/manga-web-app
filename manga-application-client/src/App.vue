05af5fc63f226523c8997da3ea9d634e2c3d5f03
prepodobnuy <zvfvbycsy6@gmail.com>
2024-09-03 17:33:03 +0300

    added footer

--- manga-application-client/src/App.vue
+++ manga-application-client/src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import topHeader from './components/topHeader.vue'
+import topFooter from './components/topFooter.vue';
 </script>
 
 <template>
-  <topHeader />
-  <header>
-    <div class="wrapper">
-
-    </div>
-  </header>
-  <RouterView class="router-view" />
-
+  <div class="wrapper">
+    <topHeader />
+    <RouterView class="router-view" />
+  </div>
+  <topFooter />
 </template>
 
 <style scoped lang="scss">
 .router-view {
   margin-top: 76px;
+}
+
+.wrapper {
+  min-height: 100vh;
 }
 </style>
---
+++ manga-application-client/src/components/topFooter.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'topFooter'
+}
+</script>
+
+<template>
+    <footer>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    margin-top: 100px;
+    height: 300px;
+    background-color: var(--fg-color);
+}
+</style>
